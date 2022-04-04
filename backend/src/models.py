4d1a21c76f712765e25a957e9cf825b098bca76f
mahdih42 <mahdih@stud.ntnu.no>
2022-04-04 10:05:50 +0200

    Add new fields

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class Article(BaseModel):
+    source: str
+    url: str
     title: str
     content: str
+    publish_date: datetime
+    polarity: int
+    image_urls: dict
+    authors: list
