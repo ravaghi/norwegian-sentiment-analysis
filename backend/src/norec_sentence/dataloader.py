6de20f7ea136b6d487d2136ca095d34c9ce47e08
mahdih42 <mahdih@stud.ntnu.no>
2022-04-11 17:34:48 +0200

    Create dataloader.py

---
+++ backend/src/norec_sentence/dataloader.py
@@ -0,0 +1,30 @@
+import json
+import pandas as pd
+
+
+def load_full_dataset() -> pd.DataFrame:
+    data = []
+    for name in ["train", "test", "dev"]:
+        with open(f"3class/{name}.json", encoding="utf-8") as file:
+            data = data + json.load(file)
+    # Convert to pandas dataframe
+    data = pd.DataFrame(data)
+    # Convert labels to numerical values, and add them to the dataframe
+    data["label"] = data["label"].replace({"Positive": 2, "Neutral": 1, "Negative": 0})
+    return data
+
+
+def load_balanced_dataset() -> pd.DataFrame:
+    data = load_full_dataset()
+
+    postives_df = data[data["label"] == 2]
+    neutrals_df = data[data["label"] == 1]
+    negatives_df = data[data["label"] == 0]
+
+    smallest_size = min(postives_df.shape[0], neutrals_df.shape[0], negatives_df.shape[0])
+
+    postives_df = postives_df.sample(n=smallest_size)
+    neutrals_df = neutrals_df.sample(n=smallest_size)
+    negatives_df = negatives_df.sample(n=smallest_size)
+
+    return pd.concat([postives_df, neutrals_df, negatives_df])
