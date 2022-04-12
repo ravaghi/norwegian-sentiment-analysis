cd8307a522be5eac675a9e4d8c97d7ea24490ab9
mahdih42 <mahdih@stud.ntnu.no>
2022-04-12 18:28:44 +0200

    Add binary data loader

--- backend/src/norec_sentence/dataloader.py
+++ backend/src/norec_sentence/dataloader.py
@@ -28,3 +28,30 @@ def load_balanced_dataset() -> pd.DataFrame:
     negatives_df = negatives_df.sample(n=smallest_size)
 
     return pd.concat([postives_df, neutrals_df, negatives_df])
+
+
+def load_binary_dataset() -> pd.DataFrame:
+    data = []
+    for name in ["train", "test", "dev"]:
+        with open(f"binary/{name}.json", encoding="utf-8") as file:
+            data = data + json.load(file)
+    # Convert to pandas dataframe
+    data = pd.DataFrame(data)
+    # Convert labels to numerical values, and add them to the dataframe
+    data["label"] = data["label"].replace({"Positive": 1, "Negative": 0})
+
+    return data
+
+
+def load_balanced_binary_dataset() -> pd.DataFrame:
+    data = load_binary_dataset()
+
+    postives_df = data[data["label"] == 1]
+    negatives_df = data[data["label"] == 0]
+
+    smallest_size = min(postives_df.shape[0], negatives_df.shape[0])
+
+    postives_df = postives_df.sample(n=smallest_size)
+    negatives_df = negatives_df.sample(n=smallest_size)
+
+    return pd.concat([postives_df, negatives_df])
