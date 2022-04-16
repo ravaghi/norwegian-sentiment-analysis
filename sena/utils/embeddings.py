0d765cbca13442e85dbef6eb91b40a86ae02f173
mahdih42 <mahdih@stud.ntnu.no>
2022-04-16 17:38:52 +0200

    Add download function for embeddings

--- sena/utils/embeddings.py
+++ sena/utils/embeddings.py
@@ -1,8 +1,42 @@
 import numpy as np
 import os
 from tqdm import tqdm
+import requests
+import zipfile
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def download_embedding() -> list:
+    """Downloads the embeddings from the url.
+
+    Returns: A list of the embeddings.
+
+    """
+    url = "http://vectors.nlpl.eu/repository/20/79.zip"
+    file_name = "79.zip"
+    file_path = os.path.join(BASE_DIR, file_name)
+
+    print("Downloading embeddings ...")
+    with open(file_path, "wb") as f:
+        f.write(requests.get(url).content)
+
+    with zipfile.ZipFile(file_path, "r") as zip_ref:
+        print("Extracting embeddings...")
+        zip_ref.extractall(BASE_DIR)
+
+    # Remove zip file and other files
+    os.remove(file_path)
+    os.remove(os.path.join(BASE_DIR, "README"))
+    os.remove(os.path.join(BASE_DIR, "model.bin"))
+    os.remove(os.path.join(BASE_DIR, "meta.json"))
+    os.rename(os.path.join(BASE_DIR, "model.txt"), os.path.join(BASE_DIR, "glove.4M.100d.txt"))
+
+    # Read text file
+    with open(os.path.join(BASE_DIR, "glove.4M.100d.txt"), encoding="utf-8") as file:
+        content = file.readlines()[1:]
+
+    return content
 
 
 def load_embeddings() -> dict:
@@ -16,9 +50,12 @@ def load_embeddings() -> dict:
     if os.path.exists(os.path.join(BASE_DIR, 'glove.4M.100d.npy')):
         return np.load(os.path.join(BASE_DIR, 'glove.4M.100d.npy'), allow_pickle=True)
 
-    # Load embeddings from text file
-    with open(os.path.join(BASE_DIR, "glove.4M.100d.txt"), encoding="utf-8") as f:
-        content = f.readlines()[1:]
+    if os.path.exists(os.path.join(BASE_DIR, 'glove.4M.100d.txt')):
+        # Load embeddings from text file
+        with open(os.path.join(BASE_DIR, "glove.4M.100d.txt"), encoding="utf-8") as f:
+            content = f.readlines()[1:]
+    else:
+        content = download_embedding()
 
     # Create dictionary of embeddings
     embeddings_index = dict()
