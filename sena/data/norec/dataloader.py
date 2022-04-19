23d422cf30fe42ce32c0ea39a345e75c119606e7
mahdih42 <mahdih@stud.ntnu.no>
2022-04-19 16:56:32 +0200

    Simplify data loading

--- sena/data/norec/dataloader.py
+++ sena/data/norec/dataloader.py
@@ -16,127 +16,96 @@ def load_metadata() -> dict:
         return json.load(file)
 
 
-def load_full_dataset() -> dict:
+def load_full_dataset(binary=False) -> dict:
     """Loads the full dataset.
 
-    Returns: A dictionary of dataframes containing the multiclass datasets.
+    Args:
+        binary: Whether to load the binary dataset.
+
+    Returns: A dictionary of dataframes containing the datasets.
 
     """
     metadata = load_metadata()
+    dataset_type = "binary" if binary else "multiclass"
     data = {}
+
     for name in ["train", "test", "dev"]:
-        if os.path.exists(os.path.join(BASE_DIR, f"data/multiclass/{name}.json")):
-            with open(os.path.join(BASE_DIR, f"data/multiclass/{name}.json"), encoding="utf-8") as file:
+        # Load existing files if they exist
+        if os.path.exists(os.path.join(BASE_DIR, f"data/{dataset_type}/{name}.json")):
+            with open(os.path.join(BASE_DIR, f"data/{dataset_type}/{name}.json"), encoding="utf-8") as file:
                 current_data = json.load(file)
         else:
             current_data = []
             current_dir = os.path.join(BASE_DIR, f"data/{name}/")
-            for file in tqdm(os.listdir(current_dir), desc=f"Loading multiclass {name} data"):
+            for file in tqdm(os.listdir(current_dir), desc=f"Loading {dataset_type} {name} data"):
                 current_file_path = os.path.join(current_dir, file)
                 current_file_id = file.split(".")[0]
                 with open(current_file_path, encoding="utf-8") as current_file:
                     label = metadata[current_file_id]["rating"]
-                    if label <= 2:
-                        label = 0
-                    elif 2 < label <= 4:
-                        label = 1
+                    if binary:
+                        if label <= 3:
+                            label = 0
+                        else:
+                            label = 1
                     else:
-                        label = 2
+                        if label <= 2:
+                            label = 0
+                        elif label == 3:
+                            label = 1
+                        else:
+                            label = 2
                     current_data.append(
                         {
                             "text": current_file.read(),
                             "label": label,
                         }
                     )
-            with open(os.path.join(BASE_DIR, f"data/multiclass/{name}.json"), "w", encoding="utf-8") as file:
+            # Save json files for later use
+            with open(os.path.join(BASE_DIR, f"data/{dataset_type}/{name}.json"), "w", encoding="utf-8") as file:
                 json.dump(current_data, file)
-        # Convert to pandas dataframe
-        data[name] = pd.DataFrame.from_dict(current_data)
+
+        # Convert to pandas dataframe and shuffle
+        data[name] = pd.DataFrame.from_dict(current_data).sample(frac=1).reset_index(drop=True)
     return data
 
 
-def load_balanced_dataset() -> dict:
-    """Loads the balanced dataset.
+def load_balanced_dataset(binary=False) -> dict:
+    """Loads the balanced (undersampled) dataset.
 
-    Returns: A dictionary of dataframes containing the multiclass datasets.
+    Args:
+        binary: Whether to load the binary dataset.
+
+    Returns: A dictionary of dataframes containing the balanced (undersampled) datasets.
 
     """
-    data = load_full_dataset()
+    data = load_full_dataset(binary=binary)
 
     balanced_data = {}
     for df_name, df in data.items():
-        postives_df = df[df["label"] == 2]
-        neutrals_df = df[df["label"] == 1]
-        negatives_df = df[df["label"] == 0]
+        if binary:
+            negatives_df = df[df["label"] == 0]
+            postives_df = df[df["label"] == 1]
+            # Find minority class
+            smallest_size = min(negatives_df.shape[0], postives_df.shape[0])
+            # Undersample majority class
+            negatives_df = negatives_df.sample(n=smallest_size)
+            postives_df = postives_df.sample(n=smallest_size)
 
-        smallest_size = min(postives_df.shape[0], neutrals_df.shape[0], negatives_df.shape[0])
-
-        postives_df = postives_df.sample(n=smallest_size)
-        neutrals_df = neutrals_df.sample(n=smallest_size)
-        negatives_df = negatives_df.sample(n=smallest_size)
-
-        result = pd.concat([postives_df, neutrals_df, negatives_df])
-        balanced_data[df_name] = result
-
-    return balanced_data
-
-
-def load_binary_dataset() -> dict:
-    """Loads the binary dataset.
-
-    Returns: A dictionary of dataframes containing the binary dataset.
-
-    """
-    metadata = load_metadata()
-    data = {}
-    for name in ["train", "test", "dev"]:
-        if os.path.exists(os.path.join(BASE_DIR, f"data/binary/{name}.json")):
-            with open(os.path.join(BASE_DIR, f"data/binary/{name}.json"), encoding="utf-8") as file:
-                current_data = json.load(file)
+            result = pd.concat([negatives_df, postives_df]).reset_index(drop=True)
         else:
-            current_data = []
-            current_dir = os.path.join(BASE_DIR, f"data/{name}/")
-            for file in tqdm(os.listdir(current_dir), desc=f"Loading binary {name} data"):
-                current_file_path = os.path.join(current_dir, file)
-                current_file_id = file.split(".")[0]
-                with open(current_file_path, encoding="utf-8") as current_file:
-                    label = metadata[current_file_id]["rating"]
-                    if label <= 3:
-                        label = 0
-                    else:
-                        label = 1
-                    current_data.append(
-                        {
-                            "text": current_file.read(),
-                            "label": label,
-                        }
-                    )
-            with open(os.path.join(BASE_DIR, f"data/binary/{name}.json"), "w", encoding="utf-8") as file:
-                json.dump(current_data, file)
-        # Convert to pandas dataframe
-        data[name] = pd.DataFrame.from_dict(current_data)
-    return data
+            negatives_df = df[df["label"] == 0]
+            neutrals_df = df[df["label"] == 1]
+            postives_df = df[df["label"] == 2]
+            # Find minority class
+            smallest_size = min(negatives_df.shape[0], neutrals_df.shape[0], postives_df.shape[0])
+            # Undersample majority class
+            negatives_df = negatives_df.sample(n=smallest_size)
+            neutrals_df = neutrals_df.sample(n=smallest_size)
+            postives_df = postives_df.sample(n=smallest_size)
 
+            # Combine and shuffle the results
+            result = pd.concat([negatives_df, neutrals_df, postives_df]).sample(frac=1).reset_index(drop=True)
 
-def load_balanced_binary_dataset() -> dict:
-    """Loads the binary dataset.
-
-    Returns: A dictionary of dataframes containing the balanced binary datasets.
-
-    """
-    data = load_binary_dataset()
-
-    balanced_data = {}
-    for df_name, df in data.items():
-        postives_df = df[df["label"] == 1]
-        negatives_df = df[df["label"] == 0]
-
-        smallest_size = min(postives_df.shape[0], negatives_df.shape[0])
-
-        postives_df = postives_df.sample(n=smallest_size)
-        negatives_df = negatives_df.sample(n=smallest_size)
-
-        result = pd.concat([postives_df, negatives_df])
         balanced_data[df_name] = result
 
     return balanced_data
