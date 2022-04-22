e5913715efe48b4c8e77d40e0b682193199f9c8f
mahdih42 <mahdih@stud.ntnu.no>
2022-04-22 18:34:38 +0200

    Simplify preprocessing

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,12 +1,8 @@
 import re
-import string
 import pandas as pd
 import json
 import os
-from nltk.corpus import stopwords as english_stopwords
 from nltk.stem import SnowballStemmer
-
-ENGLISH_STOPWORDS = set(english_stopwords.words('english'))
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -34,10 +30,9 @@ def remove_stopwords(dataframe, column_name, stopwords) -> pd.DataFrame:
     Returns: Cleaned dataframe.
 
     """
-    dataframe_copy = dataframe.copy()
-    dataframe_copy[column_name] = dataframe_copy[column_name].apply(lambda x: " ".join(
-        [word for word in x.split() if word not in stopwords and word not in ENGLISH_STOPWORDS]))
-    return dataframe_copy
+    dataframe[column_name] = dataframe[column_name].apply(lambda x: " ".join(
+        [word for word in x.split() if word not in stopwords]))
+    return dataframe
 
 
 def remove_punctuation(dataframe, column_name) -> pd.DataFrame:
@@ -52,13 +47,12 @@ def remove_punctuation(dataframe, column_name) -> pd.DataFrame:
 
     def clean(text):
         text = text.replace("\n", " ")
-        text = re.sub(r'[^a-zæøå ]+', '', text)
-        text = re.sub(r'\s\s+', ' ', text)
+        text = re.sub(r'[^a-zæøå]+', ' ', text)
+        text = re.sub(r'\s+', ' ', text)  # remove extra spaces
         return text
 
-    dataframe_copy = dataframe.copy()
-    dataframe_copy[column_name] = dataframe_copy[column_name].apply(lambda x: clean(x))
-    return dataframe_copy
+    dataframe[column_name] = dataframe[column_name].apply(lambda x: clean(x))
+    return dataframe
 
 
 def convert_to_lowercase(dataframe, column_name) -> pd.DataFrame:
@@ -71,9 +65,8 @@ def convert_to_lowercase(dataframe, column_name) -> pd.DataFrame:
     Returns: Cleaned dataframe.
 
     """
-    dataframe_copy = dataframe.copy()
-    dataframe_copy[column_name] = dataframe_copy[column_name].apply(lambda x: x.lower())
-    return dataframe_copy
+    dataframe[column_name] = dataframe[column_name].apply(lambda x: x.lower())
+    return dataframe
 
 
 def stem_words(dataframe, column_name) -> pd.DataFrame:
@@ -88,10 +81,9 @@ def stem_words(dataframe, column_name) -> pd.DataFrame:
     """
 
     stemmer = SnowballStemmer("norwegian", ignore_stopwords=False)
-    dataframe_copy = dataframe.copy()
-    dataframe_copy[column_name] = dataframe_copy[column_name].apply(lambda x: " ".join(
+    dataframe[column_name] = dataframe[column_name].apply(lambda x: " ".join(
         [stemmer.stem(word) for word in x.split()]))
-    return dataframe_copy
+    return dataframe
 
 
 def clean_text(dataframe, column_name) -> pd.DataFrame:
