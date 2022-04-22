883c124507d507332d620972bb0eaf233f78c63d
mahdih42 <mahdih@stud.ntnu.no>
2022-04-22 08:28:49 +0200

    Change random search to hyperband

--- hyperparam_tuner.py
+++ hyperparam_tuner.py
@@ -1,11 +1,12 @@
 import data.norec.dataloader as dataloader
+from utils.visualization import plot_history
 from data.dataloader import load_data
 from keras.regularizers import l1_l2
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Embedding, SpatialDropout1D
+from keras.layers import Dense, LSTM, Embedding
 from keras.optimizers import adam_v2
 from keras.callbacks import EarlyStopping
-from keras_tuner import RandomSearch
+from keras_tuner import Hyperband
 import os
 import pickle
 
@@ -15,7 +16,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def build_model(hp):
     hp_embedding_dim = hp.Int("embedding_dim", min_value=5, max_value=100, step=5)
     hp_lstm_units = hp.Int("lstm_units", min_value=8, max_value=128, step=128)
-    hp_spatial_dropout = hp.Float("spatial_dropout", min_value=0.0, max_value=0.5, step=0.05)
     hp_dropout = hp.Float("dropout", min_value=0.0, max_value=0.5, step=0.05)
     hp_l1_reg = hp.Choice("l1_regularizer", values=[0.0, 0.001, 0.005, 0.01, 0.05, 0.1])
     hp_l2_reg = hp.Choice("l2_regularizer", values=[0.0, 0.001, 0.005, 0.01, 0.05, 0.1])
@@ -23,7 +23,6 @@ def build_model(hp):
 
     model = Sequential()
     model.add(Embedding(input_dim=num_words, output_dim=hp_embedding_dim, input_length=maxlen))
-    model.add(SpatialDropout1D(rate=hp_spatial_dropout))
     model.add(LSTM(units=hp_lstm_units, dropout=hp_dropout, kernel_regularizer=l1_l2(l1=hp_l1_reg, l2=hp_l2_reg)))
     model.add(Dense(units=num_classes, activation="softmax"))
     model.compile(optimizer=adam_v2.Adam(hp_learning_rate), loss="categorical_crossentropy", metrics=["accuracy"])
@@ -33,33 +32,34 @@ def build_model(hp):
 
 if __name__ == "__main__":
     EPOCHS = 20
-    BATCH_SIZE = 128
+    BATCH_SIZE = 32
 
     dataset = dataloader.load_full_dataset()
     processed_data = load_data(dataset)
     X_train = processed_data["X_train"]
     X_val = processed_data["X_val"]
+    X_test = processed_data["X_test"]
     y_train = processed_data["y_train"]
     y_val = processed_data["y_val"]
+    y_test = processed_data["y_test"]
     maxlen = processed_data["maxlen"]
     num_classes = processed_data["num_classes"]
     num_words = processed_data["num_words"]
 
     callbacks = [
-        EarlyStopping(monitor="val_accuracy", patience=5),
-        EarlyStopping(monitor="val_loss", patience=5),
+        EarlyStopping(monitor="val_accuracy", patience=3),
+        EarlyStopping(monitor="val_loss", patience=3),
     ]
 
-    tuner = RandomSearch(
+    tuner = Hyperband(
         build_model,
         objective="val_accuracy",
-        max_trials=100,
-        executions_per_trial=1,
+        min_epochs=5,
+        max_epochs=50,
+        factor=3,
         directory=BASE_DIR,
         project_name="sena_tuner",
     )
-
-    print(tuner.search_space_summary())
 
     tuner.search(
         X_train,
@@ -72,8 +72,13 @@ if __name__ == "__main__":
 
     with open(f"tuner_results.pkl", "wb") as f:
         pickle.dump(tuner, f)
+    tuner = pickle.load(open("tuner_results.pkl", "rb"))
 
-    """tuner = pickle.load(open("tuner_results.pkl", "rb"))
-    print(tuner.get_best_hyperparameters()[0].values)
-    print(tuner.results_summary())
-    print(tuner.get_best_models()[0].summary())"""
+    best_hps = tuner.get_best_hyperparameters(num_results=1)[0]
+    model = tuner.hypermodel.build(best_hps)
+    history = model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE,
+                        validation_data=(X_val, y_val), callbacks=callbacks)
+
+    val_loss, val_acc = model.evaluate(X_test, y_test, verbose=1)
+    model.save(f"models/best_model_{val_acc}.h5")
+    plot_history(history, f"best_model_{val_acc}")
