41959fcb3b7d28784c6920c96ddb84f6b271f954
Mahdi Ravaghi <mahdi.hasani@ntnu.no>
2022-04-22 19:47:07 +0200

    Update hps choices

--- training/hps_tuner.py
+++ training/hps_tuner.py
@@ -7,7 +7,7 @@ from utils.visualization import plot_history
 from data.dataloader import load_data
 from keras.regularizers import l1_l2
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Embedding
+from keras.layers import Dense, LSTM, Embedding, SpatialDropout1D
 from keras.optimizers import adam_v2
 from keras.callbacks import EarlyStopping
 from keras_tuner import Hyperband
@@ -27,16 +27,18 @@ def build_model(hp):
     """
 
     # Defining hyperparameters
-    hp_embedding_dim = hp.Int("embedding_dim", min_value=5, max_value=100, step=5)
+    hp_embedding_dim = hp.Int("embedding_dim", min_value=8, max_value=128, step=8)
     hp_lstm_units = hp.Int("lstm_units", min_value=8, max_value=128, step=128)
     hp_dropout = hp.Float("dropout", min_value=0.0, max_value=0.5, step=0.05)
-    hp_l1_reg = hp.Choice("l1_regularizer", values=[0.0, 0.001, 0.005, 0.01, 0.05, 0.1])
-    hp_l2_reg = hp.Choice("l2_regularizer", values=[0.0, 0.001, 0.005, 0.01, 0.05, 0.1])
+    hp_spatial_dropout = hp.Float("spatial_dropout", min_value=0.0, max_value=0.5, step=0.05)
+    hp_l1_reg = hp.Choice("l1_regularizer", values=[0.0, 0.001, 0.0025, 0.005, 0.0075, 0.01, 0.012, 0.015, 0.017, 0.02])
+    hp_l2_reg = hp.Choice("l2_regularizer", values=[0.0, 0.001, 0.0025, 0.005, 0.0075, 0.01, 0.012, 0.015, 0.017, 0.02])
     hp_learning_rate = hp.Choice("learning_rate", values=[1e-2, 5e-2, 1e-3, 5e-3, 1e-4, 5e-4])
 
     # Building model
     model = Sequential()
     model.add(Embedding(input_dim=num_words, output_dim=hp_embedding_dim, input_length=maxlen))
+    model.add(SpatialDropout1D(hp_spatial_dropout))
     model.add(LSTM(units=hp_lstm_units, dropout=hp_dropout, kernel_regularizer=l1_l2(l1=hp_l1_reg, l2=hp_l2_reg)))
     model.add(Dense(units=num_classes, activation="softmax"))
     model.compile(optimizer=adam_v2.Adam(hp_learning_rate), loss="categorical_crossentropy", metrics=["accuracy"])
@@ -46,7 +48,7 @@ def build_model(hp):
 
 if __name__ == "__main__":
     EPOCHS = 20
-    BATCH_SIZE = 32
+    BATCH_SIZE = 128
 
     # Loading data
     dataset = dataloader.load_full_dataset()
